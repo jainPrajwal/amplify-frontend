3b7cf2ee9b747ff748eea752b14b96ddfa016de7
unknown <jainprajwal123@gmail.com>
2022-07-31 17:32:31 +0530

    chore: production api

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,2 @@
-// export const BASE_API = `https://amplitude-backend.herokuapp.com`;
-export const BASE_API = `http://127.0.0.1:3000`;
+export const BASE_API = `https://amplitude-backend.herokuapp.com`;
+// export const BASE_API = `http://127.0.0.1:3000`;
